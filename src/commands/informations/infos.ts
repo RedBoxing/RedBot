4448bb27a2b37dfdb209c636a0d9e6f23aa59379
RedBoxing <thomas200574@gmail.com>
2021-09-15 16:48:33 +0200

    Fixed Stuff in infos command

--- src/commands/informations/infos.ts
+++ src/commands/informations/infos.ts
@@ -3,6 +3,7 @@ import DiscordClient from "../../client/client";
 import BaseCommand from "../../structures/base/BaseCommand";
 
 import os from 'os';
+import { humanFileSize } from "../../utils/utils";
 
 export default class EmbedCommand extends BaseCommand {
     constructor() {
@@ -10,18 +11,18 @@ export default class EmbedCommand extends BaseCommand {
     }
 
     public async exec(client: DiscordClient, message: Message, args: any[]) : Promise<void> {
-        console.log("a");
        const embed = new MessageEmbed();
        embed.setAuthor("RedBot", client.user.avatarURL());
        embed.setThumbnail(client.user.avatarURL());
        embed.setFooter("RedBot by RedBoxing", (await client.users.fetch(process.env.AUTHOR_ID)).avatarURL());
 
-       embed.addField("Version", "Node.js : `16.6.1`\n Discord.js: 13.1.0\n RedBot: 1.0.1", true);
+       console.log(process.versions)
+       embed.addField("Version", "Node.js : `" + process.versions.node + "`\n Discord.js: 13.1.0\n RedBot: 1.0.1", true);
        embed.addField("Stats", "Utilisateurs : `" + client.users.cache.size + "`\n Commandes: `3`", true);
        
        const total = os.totalmem();
        const free = os.freemem();
-       embed.addField("Serveur", "CPU: `" + os.cpus()[0].model + "`\n Utilisation de la mémoire: `" + (total - free) + "/" + total + "`\n OS: `" + os.arch() + "`", false);
+       embed.addField("Serveur", "CPU: `" + os.cpus()[0].model + "`\n Utilisation de la mémoire: `" + humanFileSize(total - free) + "/" + humanFileSize(total) + "`\n OS: `" + os.platform() + "`", false);
 
        message.channel.send(embed);
     }
