f5f55c153a59d7b0ff75c5e39a3216fcf9d64a67
RedBoxing <thomas200574@gmail.com>
2021-09-15 17:22:57 +0200

    Update infos.ts

--- src/commands/informations/infos.ts
+++ src/commands/informations/infos.ts
@@ -7,24 +7,24 @@ import fs from 'fs'
 
 import { humanFileSize } from "../../utils/utils";
 
-const packageJson = fs.readFileSync('./package.json', 'utf-8');
-
 export default class EmbedCommand extends BaseCommand {
     constructor() {
         super("infos", "informations", [], []);
     }
 
-    public async exec(client: DiscordClient, message: Message, args: any[]) : Promise<void> {
-       const embed = new MessageEmbed();
-       embed.setAuthor("RedBot", client.user.avatarURL());
-       embed.setThumbnail(client.user.avatarURL());
-       embed.setFooter("RedBot by RedBoxing", (await client.users.fetch(process.env.AUTHOR_ID)).avatarURL());
+    public async exec(client: DiscordClient, message: Message, args: any[]): Promise < void > {
+        const packageJson = fs.readFileSync('./package.json', 'utf-8');
 
-       embed.addField("Version", "Node.js : `" + process.versions.node + "`\n Discord.js: `" + packageJson["dependencies"]["discord.js"] +"`\n RedBot: `" + packageJson["version"] + "`\n MariaDB: `10.5.9`", true);
-       embed.addField("Stats", "Utilisateurs : `" + client.users.cache.size + "`\n Commandes: `3`", true);
-       
-       embed.addField("Serveur", "CPU: `" + os.cpus()[0].model + "`\n Utilisation de la mémoire: `" + humanFileSize(process.memoryUsage().heapUsed) + "/" + humanFileSize(1024000000) + "`\n OS: `" + os.type() + " " + os.release() + "`", false);
+        const embed = new MessageEmbed();
+        embed.setAuthor("RedBot", client.user.avatarURL());
+        embed.setThumbnail(client.user.avatarURL());
+        embed.setFooter("RedBot by RedBoxing", (await client.users.fetch(process.env.AUTHOR_ID)).avatarURL());
 
-       message.channel.send(embed);
+        embed.addField("Version", "Node.js : `" + process.versions.node + "`\n Discord.js: `" + packageJson["dependencies"]["discord.js"] + "`\n RedBot: `" + packageJson["version"] + "`\n MariaDB: `10.5.9`", true);
+        embed.addField("Stats", "Utilisateurs : `" + client.users.cache.size + "`\n Commandes: `3`", true);
+
+        embed.addField("Serveur", "CPU: `" + os.cpus()[0].model + "`\n Utilisation de la mémoire: `" + humanFileSize(process.memoryUsage().heapUsed) + "/" + humanFileSize(1024000000) + "`\n OS: `" + os.type() + " " + os.release() + "`", false);
+
+        message.channel.send(embed);
     }
 }
