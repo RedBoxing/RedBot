8bcfc00598181f54dce7a45513c6eed410bca0d3
RedBoxing <thomas200574@gmail.com>
2021-09-15 17:06:16 +0200

    Update infos.ts

--- src/commands/informations/infos.ts
+++ src/commands/informations/infos.ts
@@ -3,7 +3,6 @@ import DiscordClient from "../../client/client";
 import BaseCommand from "../../structures/base/BaseCommand";
 
 import os from 'os';
-import v8 from 'v8'
 
 import { humanFileSize } from "../../utils/utils";
 
@@ -21,10 +20,7 @@ export default class EmbedCommand extends BaseCommand {
        embed.addField("Version", "Node.js : `" + process.versions.node + "`\n Discord.js: `13.1.0`\n RedBot: `1.0.1`", true);
        embed.addField("Stats", "Utilisateurs : `" + client.users.cache.size + "`\n Commandes: `3`", true);
        
-       console.log(v8.getHeapStatistics())
-       const total = process.memoryUsage().heapTotal;
-       const used = process.memoryUsage().heapUsed;
-       embed.addField("Serveur", "CPU: `" + os.cpus()[0].model + "`\n Utilisation de la mémoire: `" + humanFileSize(used) + "/" + humanFileSize(total) + "`\n OS: `" + os.platform() + "`", false);
+       embed.addField("Serveur", "CPU: `" + os.cpus()[0].model + "`\n Utilisation de la mémoire: `" + humanFileSize(process.memoryUsage().heapUsed) + "/" + humanFileSize(1024000000) + "`\n OS: `" + os.platform() + "`", false);
 
        message.channel.send(embed);
     }
